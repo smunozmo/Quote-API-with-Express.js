655a3e8c36029b942f46e4420fdf4bb0c415456c
Sebastián M <smunozmo@gmail.com>
2022-03-13 20:53:52 -0500

    Add GET routes.

--- server.js
+++ server.js
@@ -8,3 +8,41 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (req, res, next) => {
+    let quoteObject = {
+        quote: '',
+    };
+
+    quoteObject['quote'] = getRandomElement(quotes)
+    
+    res.send(quoteObject);
+})
+
+app.get('/api/quotes', (req, res, next) => {
+    
+    let quoteObject = {
+        quotes: '',
+    };
+    
+    if (Object.keys(req.query).length > 0) {
+        const authorQuotes = [];
+        
+        quotes.forEach(element => {
+            if (element['person'] === req.query['person']) {
+                authorQuotes.push(element)
+            }
+        });
+        quoteObject['quotes'] = authorQuotes;
+        res.send(quoteObject);
+        
+    } else {
+        quoteObject['quotes'] = quotes;
+        res.send(quoteObject);
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
+
+// console.log(quotes);
